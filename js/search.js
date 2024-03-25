392e83c8d3aa74f4cbe72d536060b993217b7b29
DQY-PC\29629 <2962951035@qq.com>
2024-03-25 22:19:29 +0800

    累了

--- js/search.js
+++ js/search.js
@@ -1,37 +1,26 @@
 const search = document.querySelector('.header .search')
 const searchextent = document.querySelector('.header .after .searchextent')
+const searchdetitle = document.querySelector('.searchextent .title')
 const historylist = document.querySelector('.searchextent .history ul')
 let control2 = 0
-let control7 = 1
 search.addEventListener('click',()=>{
   if(control2 === 0)
   {search.style.width = '200px'
   search.style.display = 'flex'
-  search.innerHTML = '<input type="text" placeholder="输入搜索内容"><button> 搜索</button>'
+  search.innerHTML = '<input type="text" placeholder="输入搜索内容" id="search"><button> 搜索</button>'
   control2 = 1
-
-  const buttonsearch = document.querySelector('.header .search button')
-  const input = document.querySelector('.header .search input')
-  input.addEventListener('focus',()=>{
-    console.log(1)
-    searchextent.style.display = 'block'
-  })
-  searchextent.addEventListener('mouseenter',()=>{
-    if(control2) control7 = 0
-    console.log('enter')
-  })
-  searchextent.addEventListener('mouseleave',()=>{
-    if(!control7) 
+  const searchinput = document.querySelector("#search")
+  function showlist(){
+    const inputValue = searchinput.ariaValueMax.trim()
+    if(inputValue.length > 0)
     {
-      control7 = 1
-      searchextent.style.display = 'none'
+      searchdetitle.style.display = 'none'
     }
-  })
-  input.addEventListener('blur',()=>{
-    console.log(2)
-    if(control7)
-    searchextent.style.display = 'none'
-  })
+    else{
+      searchdetitle.style.display = 'flex'
+    }
+  }
+  searchinput.addEventListener('focus',showlist)
   }
 })
 
